c4218d49b111bb5a9a04f0f80bb346b094ab8d3d
Frank <donghyun.seo91@gmail.com>
2022-02-27 01:58:52 +0900

    :recycle: Refactor unused lodash

--- editor/jest.config.js
+++ editor/jest.config.js
@@ -1,10 +1,8 @@
-const { cloneDeep } = require('lodash');
 const jestConfig = require('../jest.config');
-const overwrittenConfig = cloneDeep(jestConfig);
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig');
 
-overwrittenConfig.moduleNameMapper = {
+jestConfig.moduleNameMapper = {
   ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
-module.exports = overwrittenConfig;
+module.exports = jestConfig;
